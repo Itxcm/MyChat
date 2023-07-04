0f58e1f64a2a927a7703a6df2780920510ce4ceb
我的代码没有bug <1187390456@qq.com>
2023-07-05 02:30:52 +0800

    完成登录UI

--- Client/src/login/loginwidget.cpp
+++ Client/src/login/loginwidget.cpp
@@ -1,14 +1,23 @@
 #include "loginwidget.h"
 #include "ui_loginwidget.h"
+#include <QFile>
 
-LoginWidget::LoginWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::LoginWidget)
+LoginWidget::LoginWidget(QWidget *parent) : QWidget(parent),
+                                            ui(new Ui::LoginWidget)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+
+  this->setWindowFlags(Qt::FramelessWindowHint);
+  this->setAttribute(Qt::WA_TranslucentBackground);
+
+  QFile file(":qss/qss/default.css");
+  file.open(QIODevice::ReadOnly);
+
+  qApp->setStyleSheet(file.readAll());
+  file.close();
 }
 
 LoginWidget::~LoginWidget()
 {
-    delete ui;
+  delete ui;
 }
